79c0025a80560e0ba4f39ea29274441379c5d636
nagarjnasanam <snagarjuna2001@gmail.com>
2025-12-18 02:44:24 -0500

    portfolio apis implementation

--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,7 @@ import (
 func RegisterRoutes(r *gin.Engine) {
 	r.POST("/reward", controllers.CreateReward)
 	r.GET("/portfolio/:userId", controllers.GetPortfolio)
+	r.GET("/today-stocks/:userId", controllers.GetTodayStocks)
+	r.GET("/historical-inr/:userId", controllers.GetHistoricalINR)
+	r.GET("/stats/:userId", controllers.GetStats)
 }
